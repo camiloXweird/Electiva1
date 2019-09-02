9cb267b3bc86e7b6e891571bcd1b26a6419d6c7f
DiegoNaranjo913 <30935581+DiegoNaranjo913@users.noreply.github.com>
2019-09-01 22:11:04 -0500

    Comienzo Proyecto
    Se maquetea la base del laberianto con algunos materiales y script. Tambien estan creadas las carpetas y algunas funcionalidades

---
+++ ProyectoElectiva/Assets/Scripts/CamaraControler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraControler : MonoBehaviour {
+    public GameObject Jugador;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start () {
+        offset = transform.position - Jugador.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    void LateUpdate () {
+        transform.position = Jugador.transform.position + offset;
+    }
+}
---
+++ ProyectoElectiva/Assets/Scripts/JugadorControlador.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JugadorControlador : MonoBehaviour {
+    public int velocidad;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start () {
+        rb = GetComponent<Rigidbody> ();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    void FixedUpdate () {
+
+        float movimientoHorizontal = Input.GetAxis ("Horizontal");
+        float movimientoVertical = Input.GetAxis ("Vertical");
+        Vector3 movimiento = new Vector3 (movimientoHorizontal, 0.0f, movimientoVertical);
+        rb.AddForce (movimiento * velocidad);
+
+    }
+}
